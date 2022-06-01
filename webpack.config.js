89b1686d2374863d276a7f2501ce93cbc22c12b8
Veronika Rychlá <veronika.rychla108@gmail.com>
2022-06-01 18:57:55 +0200

    Simple React app

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'img/[name]-[hash:6].[ext]',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/resource'
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '', noErrorOnMissing: true },
+      ],
+    }),
+  ],
+};
