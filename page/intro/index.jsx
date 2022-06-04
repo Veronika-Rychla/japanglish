d5e63bf749c8cdc5b59592cb801920c82da9d67a
Veronika-Rychla <veronika.rychla108@gmail.com>
2022-06-04 11:08:11 +0200

    Intro page

---
+++ page/intro/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+export const Intro = () => {
+  return (
+    <>
+      <section class="intro">
+        <div class="title">JAPANGLISH</div>
+        <div class="trip">Trip to Tokyo</div>
+      </section>
+      <footer>
+        <div class="media">media</div>
+      </footer>
+    </>
+  );
+};
