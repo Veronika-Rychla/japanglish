81d81005eea8bdef2ecd45b289748d44d1740286
ronwain <100219248+ronwain@users.noreply.github.com>
2022-06-09 09:54:04 +0200

    Zpráva

--- src/GameOver/GameOver.jsx
+++ src/GameOver/GameOver.jsx
@@ -5,15 +5,19 @@ import './GameOver.css';
 export const GameOver = () => {
   return (
     <>
+      <span>
       <div className="GameOver">You were killed by a shinkansen!</div>
-      <img
+      <div><img
         className="Game__toolbar-restart"
         alt="restart"
         src="/restart.svg"
         width={50}
         height={50}
         onClick={() => window.location.reload()}
+        
       />
+      </div>
+      </span>
     </>
   );
 };
