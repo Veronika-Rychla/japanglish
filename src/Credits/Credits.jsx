516f7d41bb273a2597273b1baee633e062002394
Veronika Rychlá <veronika.rychla108@gmail.com>
2022-06-09 21:24:31 +0200

    Credits page

--- src/Credits/Credits.jsx
+++ src/Credits/Credits.jsx
@@ -3,5 +3,22 @@ import React from 'react';
 import './Credits.css';
 
 export const Credits = () => {
-  return <div className="Credits">Thank for playing!</div>;
+  return (
+    <div className="Credits">
+      <p>
+        Thanks for playing! This game was originally created as our final course
+        project for Digital Academy: Web organized by Czechitas but it gradually
+        became something bigger. We developed our own visual novel game engine
+        which we hope to keep developing further.
+      </p>
+      <p>Developers</p>
+      <p>Veronika Rychlá: JavaScript & React</p>
+      <p>Bára Ševčíková: UX design & CSS</p>
+      <p>Artist</p>
+      <p>Nikol Schirová: character design</p>
+      <p>Mentors</p>
+      <p>Ondřej Staněk</p>
+      <p>Albert Soriano</p>
+    </div>
+  );
 };
